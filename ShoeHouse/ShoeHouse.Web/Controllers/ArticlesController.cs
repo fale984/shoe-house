7c066e2b51df5b2a86b8964090d04394633e4cfb
Francisco <Francisco@DRIK-710>
2017-01-22 13:45:20 -0500

    Add controller to expose Articles.

---
+++ ShoeHouse/ShoeHouse.Web/Controllers/ArticlesController.cs
@@ -0,0 +1,32 @@
+﻿using ShoeHouse.Core.Exportables;
+using ShoeHouse.Core.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoeHouse.Web.Controllers
+{
+    public class ArticlesController : ApiController
+    {
+        public ArticleManager articlesManager = new ArticleManager();
+
+        public IEnumerable<SimpleArticle> Get()
+        {
+            return articlesManager.GetArticles();
+        }
+
+        public SimpleArticle Get(int id)
+        {
+            return articlesManager.GetArticle(id);
+        }
+
+        [Route("services/articles/stores/{id}")]
+        public IEnumerable<SimpleArticle> GetFromStore(int id)
+        {
+            return articlesManager.GetStoreArticles(id);
+        }
+    }
+}
