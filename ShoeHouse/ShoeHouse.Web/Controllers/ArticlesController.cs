7083bfe3c4246ce643086a1dabbfbf0a40915371
Francisco Lopez <fale984@gmail.com>
2017-01-23 19:06:32 -0500

    Remove old class used to wrap API responses in the custom format.

--- ShoeHouse/ShoeHouse.Web/Controllers/ArticlesController.cs
+++ ShoeHouse/ShoeHouse.Web/Controllers/ArticlesController.cs
@@ -7,41 +7,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 
 namespace ShoeHouse.Web.Controllers
 {
+    /// <summary>
+    /// Service controller to expose Article items
+    /// Requires basic authentication for access
+    /// Returns objects with wrapper according to the requirements
+    /// </summary>
     [BasicAuthentication]
-    public class ArticlesController : ExtendedResponseController
+    public class ArticlesController : ApiController
     {
         public ArticleManager articlesManager = new ArticleManager();
 
+        /// <summary>
+        /// List all Article items
+        /// </summary>
+        /// <returns>Article list</returns>
         public ArticlesResponse Get()
         {
             var articles = articlesManager.GetArticles();
-            //return Success("articles", articles);
+
             return new ArticlesResponse(articles);
         }
 
+        /// <summary>
+        /// Find Article by id
+        /// </summary>
+        /// <param name="id">Id to search</param>
+        /// <returns>Article if found, or custom error</returns>
         public ArticleResponse Get(int id)
         {
             var article = articlesManager.GetArticle(id);
 
+            //If the article does not exists, return custom error
             if (article == null)
             {
                 throw new CustomHttpException(404);
             }
 
-            //return Success("article", article);
             return new ArticleResponse(article);
         }
 
+        /// <summary>
+        /// List all Articles in the store specified
+        /// </summary>
+        /// <param name="id">Id of the store</param>
+        /// <returns>Article list</returns>
         [Route("services/articles/stores/{id}")]
         public ArticlesResponse GetFromStore(int id)
         {
             var articles = articlesManager.GetStoreArticles(id);
-            //return Success("articles", articles);
+
             return new ArticlesResponse(articles);
         }
     }
--- ShoeHouse/ShoeHouse.Web/Controllers/StoresController.cs
+++ ShoeHouse/ShoeHouse.Web/Controllers/StoresController.cs
@@ -11,28 +11,42 @@ using System.Web.Http;
 
 namespace ShoeHouse.Web.Controllers
 {
+    /// <summary>
+    /// Service controller to expose Store items
+    /// Requires basic authentication for access
+    /// Returns objects with wrapper according to the requirements
+    /// </summary>
     [BasicAuthentication]
-    public class StoresController : ExtendedResponseController
+    public class StoresController : ApiController
     {
         private StoresManager storesManager = new StoresManager();
 
+        /// <summary>
+        /// List all Store items
+        /// </summary>
+        /// <returns>Store list</returns>
         public StoresResponse Get()
         {
             var stores = storesManager.GetStores();
-            //return Success("stores", stores);
+
             return new StoresResponse(stores);
         }
 
+        /// <summary>
+        /// Find Store by Id
+        /// </summary>
+        /// <param name="id">Id to search</param>
+        /// <returns>Store if found, or custom error</returns>
         public StoreResponse Get(int id)
         {
             var store = storesManager.GetStore(id);
 
+            //If the store does not exists, return custom error
             if (store == null)
             {
                 throw new CustomHttpException(404);
             }
 
-            //return Success("store", store);
             return new StoreResponse(store);
         }
     }
